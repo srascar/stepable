a5cffaa8f75242aaebebaa44ec39922fae668262
Sylvain <sylvain.rascar@ekino.com>
2020-01-29 16:23:14 +0000

    feat: apply transition in a callback

--- src/stepable.ts
+++ src/stepable.ts
@@ -18,7 +18,8 @@ interface Options {
   ) => boolean;
   beforeTransition: (
     transitionEvent: TransitionEvent,
-    event: MouseEvent
+    event: MouseEvent,
+    cb: (transitionEvent: TransitionEvent) => any
   ) => any;
   afterTransition: (transitionEvent: TransitionEvent, event: MouseEvent) => any;
 }
@@ -31,57 +32,25 @@ const defaultOptions: Options = {
   triggerSelector: ".stepable-trigger",
   prevTriggerSelector: ".stepable-trigger--prev",
   shouldTransition: (transitionEvent, event) => true,
-  beforeTransition: (transitionEvent, event) => true,
+  beforeTransition: (transitionEvent, event, cb) => cb(transitionEvent),
   afterTransition: (transitionEvent, event) => true
 };
 
 export default class Stepable {
   element: HTMLElement;
   currentIndex: number;
-  steps: HTMLElement[];
+  steps: HTMLElement[] = [];
   options: Options;
 
   constructor(element: HTMLElement, options: Partial<Options> = {}) {
     this.element = element;
     this.options = { ...defaultOptions, ...options };
     this.currentIndex = this.options.initialStep;
-    this.steps = Array.from(
-      element.querySelectorAll(this.options.stepSelector)
-    );
-    this.attachCallbacks();
+    this.refresh();
     if (this.steps[this.currentIndex]) {
       this.steps[this.currentIndex].classList.add(
         `${this.options.activeClass}--true`
       );
-    }
-  }
-
-  transitionToStep(from: number, to: number, event: MouseEvent) {
-    const fromStep = this.steps[from];
-    const toStep = this.steps[to];
-
-    if (!fromStep || !toStep) {
-      if (to === this.steps.length) {
-        this.currentIndex = to - 1;
-      }
-      return;
-    }
-
-    const transitionEvent = {
-      from,
-      to,
-      fromStep,
-      toStep
-    };
-
-    if (this.options.shouldTransition(transitionEvent, event)) {
-      this.options.beforeTransition(transitionEvent, event);
-      fromStep.classList.remove(`${this.options.activeClass}--true`);
-      fromStep.classList.add(`${this.options.activeClass}--false`);
-      this.currentIndex = to;
-      toStep.classList.remove(`${this.options.activeClass}--false`);
-      toStep.classList.add(`${this.options.activeClass}--true`);
-      this.options.afterTransition(transitionEvent, event);
     }
   }
 
@@ -106,5 +75,52 @@ export default class Stepable {
         this.transitionToStep(this.currentIndex, this.currentIndex - 1, e);
       };
     });
+  }
+
+  refresh() {
+    this.steps = Array.from(
+      this.element.querySelectorAll(this.options.stepSelector)
+    );
+
+    this.attachCallbacks();
+  }
+
+  transitionToStep(from: number, to: number, event: MouseEvent) {
+    const fromStep = this.steps[from];
+    const toStep = this.steps[to];
+
+    if (!fromStep || !toStep) {
+      if (to === this.steps.length) {
+        this.currentIndex = to - 1;
+      }
+      return;
+    }
+
+    const transitionEvent = {
+      from,
+      to,
+      fromStep,
+      toStep
+    };
+
+    if (this.options.shouldTransition(transitionEvent, event)) {
+      const cb = (internalTransistionEvent: TransitionEvent) => {
+        internalTransistionEvent.fromStep.classList.remove(
+          `${this.options.activeClass}--true`
+        );
+        internalTransistionEvent.fromStep.classList.add(
+          `${this.options.activeClass}--false`
+        );
+        this.currentIndex = to;
+        internalTransistionEvent.toStep.classList.remove(
+          `${this.options.activeClass}--false`
+        );
+        internalTransistionEvent.toStep.classList.add(
+          `${this.options.activeClass}--true`
+        );
+        this.options.afterTransition(internalTransistionEvent, event);
+      };
+      this.options.beforeTransition(transitionEvent, event, cb);
+    }
   }
 }
